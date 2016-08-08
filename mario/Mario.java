c0a49c04fb2a8fa3d77b2844c8326b751fc204a5
victor diaz <vdmpeniel@yahoo.es>
2016-08-08 10:14:30 -0400

    "Spelling"

--- mario/Mario.java
+++ mario/Mario.java
@@ -5,7 +5,7 @@ public class Mario {
     public static void  main(String[] args){
         UserInterface ui = new UserInterface(); // Calling UI
         Pyramid py = new Pyramid(ui.inputHeight()); // Creating pyramid       
-        if(ui.inputOutputMethod() == 1) { py.setOutputStrategy(new FileOutputStrategy()); } // Setting output method if diferent than printing
-        py.postOutput(); //Outputing the pyramid, using the method selected 
+        if(ui.inputOutputMethod() == 1) { py.setOutputStrategy(new FileOutputStrategy()); } // Setting output method if different than printing
+        py.postOutput(); //Outputting the pyramid, using the method selected 
     }
 }
