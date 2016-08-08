65d30ba0078b5444820a1bad0f00cb4d286c96a8
victor diaz <vdmpeniel@yahoo.es>
2016-08-08 10:11:36 -0400

    "Adding some more commentaries"

--- mario/Mario.java
+++ mario/Mario.java
@@ -3,9 +3,9 @@ import mario.outputStrategies.FileOutputStrategy;
 
 public class Mario {
     public static void  main(String[] args){
-        UserInterface ui = new UserInterface();
-        Pyramid py = new Pyramid(ui.inputHeight());        
-        if(ui.inputOutputMethod() == 1) { py.setOutputStrategy(new FileOutputStrategy()); }
-        py.postOutput();
+        UserInterface ui = new UserInterface(); // Calling UI
+        Pyramid py = new Pyramid(ui.inputHeight()); // Creating pyramid       
+        if(ui.inputOutputMethod() == 1) { py.setOutputStrategy(new FileOutputStrategy()); } // Setting output method if diferent than printing
+        py.postOutput(); //Outputing the pyramid, using the method selected 
     }
 }
