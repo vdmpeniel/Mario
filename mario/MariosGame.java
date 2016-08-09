3d605814e33efdf2fb6a7e59b5b8b7dd6c9b9e0e
vic <vdmpeniel@yahoo.es>
2016-08-08 22:37:58 -0400

    User Interface also can be injected

--- mario/MariosGame.java
+++ mario/MariosGame.java
@@ -1,17 +1,17 @@
 package mario;
 public class MariosGame {   
     MarioSingleton mainCharacter;
-    MariosGame(MarioSingleton mainCharacter){
+    UserInterface ui;
+    MariosGame(MarioSingleton mainCharacter, UserInterface ui){
         this.mainCharacter = mainCharacter;
+        this.ui = ui; // Calling UI 
     }
     public static void  main(String[] args){
-        MariosGame newGame = new MariosGame(MarioSingleton.getInstance());
+        MariosGame newGame = new MariosGame(MarioSingleton.getInstance(), new UserInterface());
         newGame.play();
     }
     
-    public void play(){
-        UserInterface ui;
-        ui = new UserInterface(); // Calling UI       
+    public void play(){              
         mainCharacter.makePyramid(ui.height(), ui.outputMethod()); 
         //System.out.println("This object have an ID od: " + System.identityHashCode(mario));
     }
