d8c6402351ba56e12db554286ea7252085e4f471
victor diaz <vdmpeniel@yahoo.es>
2016-08-31 18:42:41 -0400

    better comparator, duh!

--- Greedy/src/main/java/CoinSetFactory/ReverseComparator.java
+++ Greedy/src/main/java/CoinSetFactory/ReverseComparator.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 //Reverse sorting order for the coinSet TreeMap
 public class ReverseComparator implements Comparator<Integer> {
     public int compare(Integer int1, Integer int2) {
-        if(int1.compareTo(int2) < 1){return 1;}
-        if(int1.compareTo(int2) > 1){return -1;}
-        return 0;
+        return int2.compareTo(int1);
     }
 }
